cce90e86ffff9d0a6894cc2734fef4f762e22b52
Naveen N K <152235303+nknaveengowda@users.noreply.github.com>
2024-01-01 18:15:27 +0530

    cucumber commit

---
+++ phase2-cucumber-maven-test/src/test/java/com/simplilearn/bddtest/testrunner/AmazonTestRunner.java
@@ -0,0 +1,14 @@
+package com.simplilearn.bddtest.testrunner;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/resources/Features/amazon-product-search2.feature", 
+	glue = {"com/simplilearn/bddtest/stepdefinitions"},
+	plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"})
+public class AmazonTestRunner {
+
+}
